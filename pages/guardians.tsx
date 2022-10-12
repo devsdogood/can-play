32f85ee76038bf9d121724659ec8f238d45208a1
Connor Fogarty <connorflynnfogarty@gmail.com>
2022-10-12 14:24:32 -0500

    Remove flex for guardians table

--- pages/guardians.tsx
+++ pages/guardians.tsx
@@ -4,15 +4,15 @@ import * as _ from "lodash";
 import Table from "../components/Table";
 
 const columns: GridColDef[] = [
-  { field: "Name", headerName: "Name", valueGetter: (params) => params.row.first_name + " " + params.row.last_name, flex: 1 },
-  { field: "first_name", headerName: "First Name", editable: true, flex: 1 },
-  { field: "last_name", headerName: "Last Name", editable: true, flex: 1 },
-  { field: "email", headerName: "Email", editable: true, flex: 1 },
-  { field: "address", headerName: "Address", editable: true, flex: 1 },
-  { field: "employer", headerName: "Employer", editable: true, flex: 1 },
-  { field: "paymentmethod", headerName: "Payment method", editable: true, flex: 1 },
-  { field: "mailchimp", headerName: "Mailchimp", editable: true, flex: 1 },
-  { field: "notes", headerName: "Notes", editable: true, flex: 1 },
+  { field: "Name", headerName: "Name", valueGetter: (params) => params.row.first_name + " " + params.row.last_name },
+  { field: "first_name", headerName: "First Name", editable: true },
+  { field: "last_name", headerName: "Last Name", editable: true },
+  { field: "email", headerName: "Email", editable: true },
+  { field: "address", headerName: "Address", editable: true },
+  { field: "employer", headerName: "Employer", editable: true },
+  { field: "paymentmethod", headerName: "Payment method", editable: true },
+  { field: "mailchimp", headerName: "Mailchimp", editable: true },
+  { field: "notes", headerName: "Notes", editable: true },
 ];
 
 export async function getServerSideProps() {
