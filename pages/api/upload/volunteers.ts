b25821feb332a049ef8697ce86890d53b022ebf8
Connor Fogarty <connorflynnfogarty@gmail.com>
2022-10-12 03:10:31 -0500

    Add volunteer endpoint

--- pages/api/upload/volunteers.ts
+++ pages/api/upload/volunteers.ts
@@ -1,8 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { hasFileUpload, toJsonRecords } from "../../../lib/file-upload-util";
-import * as mongo from "../../../lib/mongodb";
-import * as model from "../../../model/event-registration-db";
+import { PrismaClient } from '@prisma/client'
+import { saveUploadedFiles } from "../../../lib/busboy";
+import * as _ from "lodash";
 
 interface ResponseData {
   message: string;
@@ -14,35 +15,126 @@ export default async function handler(
 ) {
   if (hasFileUpload(req)) {
     try {
-      const client = await mongo.connectToDB();
+      const prisma = new PrismaClient()
+      // TODO: consolidate into one call
+      const fields = (await saveUploadedFiles(_.cloneDeep(req))).fields;
       const records = await toJsonRecords(req);
 
-      for (const record of records) {
-        const volunteer: model.Volunteer = {
-          name: record["Who"]?.split(" ") || [],
-          start_time: record["Start Time"],
-          end_time: record["End Time"],
-          date: record["Date"],
-          email: record["Email"],
-          phone_number: record["Phone"],
-        };
-        const existingVolunteer = await mongo.findVolunteer(client, volunteer);
-        const volunteer_id = existingVolunteer
-          ? existingVolunteer?._id.toHexString()
-          : (
-              await mongo.storeVolunteer(client, volunteer)
-            ).insertedId.toHexString();
+      // find program based on year, activity, and location
+      let program = await prisma.programs.findFirst({
+        where: {
+          year: parseInt(fields["year"]),
+          activity: fields["sport"],
+          location: fields["location"],
+        },
+        include: {
+          slots: true,
+        }
+      });
+
+      // create program if doesn't exist
+      if (!program) {
+        program = await prisma.programs.create({
+          data: {
+            year: parseInt(fields["year"]),
+            activity: fields["sport"],
+            location: fields["location"],
+            address: "Somewhere", // TODO!
+          },
+          include: {
+            slots: true,
+          }
+        })
       }
-      // TODO: find event based on CSV and add volunteer to event
+
+      // iterate over each registration
+      for (const record of records) {
+        const slotData = {
+          start: `${record["Date"]} ${record["Start Time"]}`,
+          end: `${record["Date"]} ${record["End Time"]}`,
+        };
+
+        // find slot with start/end dates
+        let slot = await prisma.event_slots.findFirst({
+          where: {
+            start_date: slotData.start,
+            end_date: slotData.end,
+            programs: {
+              some: {
+                id: program.id,
+              }
+            }
+          }
+        });
+
+        // create slot if doesn't already exist
+        if (!slot) {
+          slot = await prisma.event_slots.create({
+            data: {
+              start_date: slotData.start,
+              end_date: slotData.end,
+            },
+          })!;
+        }
+
+        // upsert volunteer data
+        const volunteerData = _.pick(record, ["First Name", "Last Name", "Email", "Phone"]);
+        const volunteer = await prisma.volunteers.upsert({
+          where: {
+            email: volunteerData["Email"],
+          },
+          create: {
+            email: volunteerData["Email"],
+            first_name: volunteerData["First Name"],
+            last_name: volunteerData["Last Name"],
+            phone: volunteerData["Phone"],
+            address: "Address", // TODO
+            age: 30, // TODO
+            background_check_date: "10/10/2020", // TODO
+            criminal_history: "None",
+            employer: "None", // TODO
+            notes: "None",
+            refused_participation: false,
+            school: "None",
+            signed_waiver: true,
+            volunteer_preference: "None",
+          },
+          // TODO: don't copy all these fields
+          update: {
+            first_name: volunteerData["First Name"],
+            last_name: volunteerData["Last Name"],
+            phone: volunteerData["Phone"],
+          }
+        });
+
+        // add volunteer to slot
+        await prisma.event_slots.update({
+          where: {
+            id: slot.id,
+          },
+          data: {
+            volunteers: {
+              push: {
+                volunteerId: volunteer.id,
+                attended: false,
+              }
+            },
+            programs: {
+              connect: {
+                id: program.id,
+              }
+            }
+          }
+        })
+      }
 
       res.status(200).send({ message: "Success" });
     } catch (e) {
-      console.error(`Unexpected error in /api/form-data: ${e}`);
+      console.error(`Unexpected error in /api/upload/volunteers: ${e}`);
       res.status(500).send({ message: "Internal Server Error" });
     }
   } else {
     res.status(400).send({ message: "Bad Request" });
-    
   }
 }
 
